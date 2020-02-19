8a04825d50100c4cd06c7f4775bb720ff39f81a4
Jonathan <jonakes@student.chalmers.se>
2020-02-19 11:33:39 +0100

    uppdaterat

--- U_1b.m
+++ U_1b.m
@@ -7,3 +7,15 @@ M=200;
 t=T*(0:M-1)/M;
 y=sin(w*t);
 plot(t,y);
+
+a_0=0;
+a_k=0;
+stot=a_0;
+
+for k=1:2:20
+    b_k=4/(pi*k);
+    stot=stot+a_k*cos(k*w*t)+b_k*sin(k*w*t);
+end
+hold on;
+plot(t,stot);
+
